62c4a7343bbf1c2d22d5a9b245ca8d76dcaed1b8
Lossless <github@philkunz.com>
2016-03-22 22:26:20 +0000

    add TRAVIS_TAG

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ console.log("type is: " + typeof process.env.TRAVIS_JOB_NUMBER);
 console.log("process.env.TRAVIS_TAG")
 console.log("value is: " + process.env.TRAVIS_TAG);
 console.log("type is: " + typeof process.env.TRAVIS_TAG);
+
+setTimeout(function(){ alert("Done!"); }, 5000);
